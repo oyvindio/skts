336911f7a7bba24bcdb84d81a5bc076e294617e2
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2009-06-11 16:22:19 +0200

    Fixed an issue with Ctrl,Alt,Meta and Shift
    KeyboardEvents that are fired when Ctrl, Alt, Meta or Shift is pressed
    is ignored to avoid conflict with other key bindings - such as Alt+n to
    go to tab number n in Linux, or Ctrl+n to do the same in Windows.

--- src/chrome/content/skts.js
+++ src/chrome/content/skts.js
@@ -6,7 +6,8 @@ var skts_prevTabKey = skts_prefService.getCharPref("prevTabKey");
 var skts_nextTabKey = skts_prefService.getCharPref("nextTabKey");
 
 /**
- * Returns true if we should ignore KeyboardEvents, false otherwise.
+ * Returns true if focus is on an element which takes some sort of input. in
+ * that case, we do not want to catch key presses.
  */
 function skts_inputFocus() {
     var skts_inputFocus = false;
@@ -23,16 +24,23 @@ function skts_inputFocus() {
         }
     }
 
-    /*
     var skts_window = document.commandDispatcher.focusedWindow;
     if (skts_window) {
         if(skts_window.document.designMode === "on") {
             skts_inputFocus = true;
         }
     }
-    */
 
     return skts_inputFocus;
+}
+
+/**
+ * Stop an Event from being propagated further. Called when an Event has been
+ * handled.
+ */
+function skts_stopEvent(evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
 }
 
 /**
@@ -40,24 +48,31 @@ function skts_inputFocus() {
  */
 function skts_switchTab(evt) {
 
+    // if an input element has focus, we want to let the user input text into
+    // the element, thus we return before the event is handled.
     if (skts_inputFocus()) {
         return;
     }
 
-    evt = evt || event
-
-    dump("charCode: " + evt.charCode + "\n");
-    dump("which: " + evt.which + "\n");
-    dump("skts_prevTabKey:" + skts_prevTabKey.charCodeAt(0) + "\n");
-    dump("skts_nextTabKey:" + skts_nextTabKey.charCodeAt(0) + "\n");
+    // we only want to handle single key presses. to avoid interfering with
+    // other key bindings, we return without handling the event if either of
+    // Shift, Ctrl, Alt or Meta is pressed.
+    if (evt.shiftKey
+    ||  evt.ctrlKey
+    ||  evt.altKey
+    ||  evt.metaKey) {
+        return;
+    }
 
     skts_keyPressed = evt.which
 
     if (skts_keyPressed === skts_prevTabKey.charCodeAt(0)) {
         gBrowser.mTabContainer.advanceSelectedTab(-1, true);
+        skts_stopEvent(evt);
     }
     else if (skts_keyPressed === skts_nextTabKey.charCodeAt(0)) {
         gBrowser.mTabContainer.advanceSelectedTab(1, true);
+        skts_stopEvent(evt);
     }
 }
 
