5e8f3ca6fd8701fe2534587fde8298af1ed3392c
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2009-06-11 12:27:44 +0200

    mkxpi.sh now puts xpi version in filename

--- mkxpi.sh
+++ mkxpi.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
-XPI="skts.xpi"
-SRC_DIR="src"
+# Creates a XPI from the files in $SRC_DIR.
+# Dependencies: bash, rm, sed, grep, zip
 
-# delete old xpi
-if [ -f "$XPI" ]
+NAME="single_key_tab_switch"
+SRC_DIR="src"
+VERSION=$(grep em:version "$SRC_DIR"/install.rdf | sed -e 's/<[^>]*>//g')
+VERSION=${VERSION// /} # trim whitespace
+
+# remove old xpi if it exists
+if [ -f "$NAME-$VERSION-fx.xpi" ]
 then
-    rm -vf "$XPI"
+    rm -vf "$NAME-$VERSION-fx.xpi"
 fi
 
 # create new xpi
 if [ -d "$SRC_DIR" ]
 then
-    pushd src
+    pushd $SRC_DIR
 
     # this is just to keep track of when the xpi was packaged
     pack_date=$(date +%Y%m%d%H%M%S)
     touch packaged-"$pack_date"
 
-    zip -r ../"$XPI" ./*
+    zip -r ../"$NAME-$VERSION-fx.xpi" ./*
 
     rm -vf packaged-"$pack_date"
 
